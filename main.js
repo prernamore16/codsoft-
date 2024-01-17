e0ebddcc07e5771ff564336d9d0f30b266bb5620
prernamore16 <153173494+prernamore16@users.noreply.github.com>
2024-01-17 21:11:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#element', {
+      strings: ['Frontend Developer', ' &amp; a Web Developer.'],
+      typeSpeed: 100,
+      backspeed: 100,
+      backDelay:1000,
+      loop: true
+});
